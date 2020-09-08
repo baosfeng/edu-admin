5282aa51f3f530dd0d65ebd2b405525b1ebe1ebd
bsfeng <bsfeng@foxmail.com>
2020-09-08 17:50:12 +0800

    讲师列表和讲师添加页面

---
+++ src/api/edu/teacher.js
@@ -0,0 +1,41 @@
+import request from '@/utils/request'
+
+const api_name = '/edu/teacher'
+export default {
+
+    getPageList(page, limit, searchObj) {
+        return request({
+            url: `${api_name}/page/query/${page}/${limit}`,
+            method: 'post',
+            // 需要转为json使用data,需要使用参数方式使用param
+            data: searchObj
+        })
+    },
+
+    deleteTeacherById(id) {
+        return request({
+            url: `${api_name}/${id}`,
+            method: 'delete',
+        })
+    },
+    save(teacher) {
+        return request({
+            url: `${api_name}/save`,
+            method: 'post',
+            data: teacher
+        })
+    },
+    getById(id) {
+        return request({
+            url: `${api_name}/${id}`,
+            method: 'get'
+        })
+    },
+    updateById(teacher) {
+        return request({
+            url: `${api_name}/${teacher.id}`,
+            method: 'put',
+            data: teacher
+        })
+    }
+}
