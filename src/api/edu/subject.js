b0002d8385b3fde2aed2c830e741f5f8410c7c52
bsfeng <bsfeng@foxmail.com>
2020-09-09 10:46:04 +0800

    添加课程分类树形结构展示

---
+++ src/api/edu/subject.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+const api_name = '/edu_teacher/subject'
+
+export default {
+
+    getNestedTreeList() {
+        return request({
+            url: `${api_name}`,
+            method: 'get'
+        })
+    }
+}
