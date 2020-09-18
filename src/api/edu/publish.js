056049ed6ad99d7416e41a3d776f6a6bdf266f7e
bsfeng <bsfeng@foxmail.com>
2020-09-18 19:06:14 +0800

    添加课程最终发布开发

---
+++ src/api/edu/publish.js
@@ -0,0 +1,19 @@
+import request from '@/utils/request'
+
+const api_name = '/edu_teacher/course'
+
+export default {
+    getCoursePublishInfoById(id) {
+        return request({
+            url: `${api_name}/course-publish-info/${id}`,
+            method: 'get'
+        })
+    },
+
+    publishCourse(id) {
+        return request({
+            url: `${api_name}/publish-course/${id}`,
+            method: 'put'
+        })
+    }
+}
